efac555bd3fc35c2ecae37b4a27d08f5b66cc73c
Anucet Murhula <anicetmurhula20@gmail.com>
2022-08-18 08:25:14 +0200

    Update messages

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,3 +1,8 @@
 class Api::MessagesController < ApplicationController
-  
+  def index
+    @message = Message.order(Arel.sql('RANDOM()')).first.message
+    render json: {
+      message: @message
+    }
+  end
 end
