6aaf584ffceddc40c28e1c0f01ee7242f89498df
Anucet Murhula <anicetmurhula20@gmail.com>
2022-09-01 09:29:19 +0200

    Add api endpoints

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::GreetingsController < ApplicationController
+    def index
+      @greetings = Greeting.all
+      if @greetings.empty?
+        json_response({ msg: 'No Greetings at this time' }, 400)
+      else
+        render json: @greetings[rand(0...@greetings.length - 1)]
+      end
+    end
+  end
+  
